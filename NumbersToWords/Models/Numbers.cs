3b69ca1080f777f23e634d6db5ff8d5fc7e24628
Anastasia Han <anastasia.han@yahoo.com>
2022-02-07 23:39:46 -0500

    Numbers 0 to 9 works

---
+++ NumbersToWords/Models/Numbers.cs
@@ -0,0 +1,79 @@
+using System;
+using System.Collections.Generic;
+
+namespace NumbersToWords.Models
+{
+  public class Number
+  {
+    public static Dictionary<string, string> digit1 = new Dictionary<string, string> ()
+    {
+      {"0", ""},
+      {"1", "one"},
+      {"2", "two"},
+      {"3", "three"},
+      {"4", "four"},
+      {"5", "five"},
+      {"6", "six"},
+      {"7", "seven"},
+      {"8", "eight"},
+      {"9", "nine"},
+      {"10", "ten"},
+      {"11", "eleven"},
+      {"12", "twelve"},
+      {"13", "thirteen"},
+      {"14", "fourteen"},
+      {"15", "fifteen"},
+      {"16", "sixteen"},
+      {"17", "seventeen"},
+      {"18", "eightteen"},
+      {"19", "nineteen"},
+    };
+    public static Dictionary<string, string> digit2 = new Dictionary<string, string>()
+    {
+      {"0", ""},
+      {"1", ""},
+      {"2", "twenty"},
+      {"3", "thirty"},
+      {"4", "fourty"},
+      {"5", "fifty"},
+      {"6", "sixty"},
+      {"7", "seventy"},
+      {"8", "eighty"},
+      {"9", "ninety"},
+    };
+    // public static Dictionary<int, string> suffix = new Dictionary<int, string>()
+    // {
+    //   {4, "thousand"},
+    //   {7, "million"},
+    //   {10, "billion"},
+    // };
+    public static string NumberConvert(string inputNum)
+    {
+      // int inputInt = int.Parse(inputNum);
+      return digit1[inputNum];
+      // string Tens(string number)
+      // {
+      //   string numberToPass = number.Remove(0, 1);
+      //   if (int.Parse(number) >= 20)
+      //   {
+      //     string lastDigit = Ones(numberToPass);
+      //     string secondLastDigit = digit2[number[0].ToString()];
+      //     if (secondLastDigit != "0")
+      //     {
+      //       secondLastDigit += " ";
+      //     }
+      //     string result = $"{secondLastDigit}{lastDigit}";
+      //     return result;
+      //   }
+      //   else if (number[0].ToString() == "0")
+      //   {
+      //     return Ones(numberToPass);
+      //   }
+      //   else
+      //   {
+      //     return Ones(number);
+      //   }
+      // }
+    }
+  }
+}
